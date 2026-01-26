d5d05e52b28b7853eacf503306bc0d1510071759
Vaelrion <krishkundariya27@gmail.com>
2026-01-26 15:50:06 +0530

    add google sign in for login page.2

--- pulseboard_mobile/src/api/client.ts
+++ pulseboard_mobile/src/api/client.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const ipAddress = '172.31.26.213';
 
 const api = axios.create({
-  baseURL: "http://${ipAddress}:3000", // NOT localhost for mobile
+  baseURL: `http://${ipAddress}:3000`, // NOT localhost for mobile
   headers: {
     "Content-Type": "application/json",
   },
